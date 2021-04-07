4a2481f939a6af9549cb64b5599149a5c5c39df9
Doyal-N <coin20@protonmail.com>
2021-04-07 15:20:52 +0500

    refact users#show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,8 +12,8 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @questions = @user.questions
-    @questions_count = @questions.count
-    @answered_questions_count = @questions.count { |q| !q.answer.nil? }
+    @questions_count = @questions.length
+    @answered_questions_count = @user.questions.where.not(answer: nil).count
     @unanswered_questions_count = @questions_count - @answered_questions_count
   end
 end
