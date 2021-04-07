e6d4757dfdfe77d11dc10517e41768f7a9a6c5e0
Doyal-N <coin20@protonmail.com>
2021-04-07 11:55:49 +0500

    controllers

--- config/application.rb
+++ config/application.rb
@@ -34,7 +34,7 @@ module Askme
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     config.i18n.default_locale = :ru
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = 'Asia/Yekaterinburg'
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Don't generate system test files.
