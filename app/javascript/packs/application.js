85c94e8ac19d27744a45391008fe4dd15903f025
Doyal-N <coin20@protonmail.com>
2021-04-24 09:44:55 +0500

    change assets to webpack

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,13 +2,15 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+import "stylesheets/application"
+import "@fortawesome/fontawesome-free/css/all"
+import "bulma/bulma"
+const images = require.context('../images', true)
 
 import Rails from "@rails/ujs"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import 'packs/users'
-import "@fortawesome/fontawesome-free/css/all"
-import "bulma/bulma"
 
 Rails.start()
 ActiveStorage.start()
